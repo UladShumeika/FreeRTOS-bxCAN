c62adb3122db588c8d3792f09878e843944b008c
Vlad Shumeiko <wladshumeiko228@gmail.com>
2022-12-22 18:54:15 +0100

    [bxCAN.c] feat: add bxCAN_GPIO_init function

--- STM32+FreeRTOS+bxCAN/Core/Src/bxCAN.c
+++ STM32+FreeRTOS+bxCAN/Core/Src/bxCAN.c
@@ -7,6 +7,13 @@
 // Defines
 //---------------------------------------------------------------------------
 
+// CAN1 interrupt priorities
+#define CAN1_RX0_PREEMPPRIORITY					(5U)
+#define CAN1_RX0_SUBPRIORITY					(0U)
+
+#define CAN1_SCE_PREEMPPRIORITY					(6U)
+#define CAN1_SCE_SUBPRIORITY					(0U)
+
 //---------------------------------------------------------------------------
 // Typedefs
 //---------------------------------------------------------------------------
@@ -15,6 +22,7 @@ CAN_HandleTypeDef hcan1;
 //---------------------------------------------------------------------------
 // Static function prototypes
 //---------------------------------------------------------------------------
+static void bxCAN_GPIO_init(void);
 
 //---------------------------------------------------------------------------
 // Descriptions of FreeRTOS elements
@@ -46,6 +54,37 @@ void InterruptHandlingRxFIFO0Task(void const* argument)
 //---------------------------------------------------------------------------
 // Initialization functions
 //---------------------------------------------------------------------------
+
+/**
+  * @brief  GPIO configuration for bxCAN module
+  * @param  None
+  * @retval None
+  */
+static void bxCAN_GPIO_init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	__HAL_RCC_CAN1_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	// CAN1 GPIO Configuration
+	// PA11	------> CAN1_RX
+	// PA12 ------> CAN1_TX
+	GPIO_InitStruct.Pin 				= GPIO_PIN_11 | GPIO_PIN_12;
+	GPIO_InitStruct.Mode 				= GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull 				= GPIO_NOPULL;
+	GPIO_InitStruct.Speed 				= GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Alternate 			= GPIO_AF9_CAN1;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	// CAN1 interrupt init
+	HAL_NVIC_SetPriority(CAN1_RX0_IRQn, CAN1_RX0_PREEMPPRIORITY, CAN1_RX0_SUBPRIORITY);
+	HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
+
+	HAL_NVIC_SetPriority(CAN1_SCE_IRQn, CAN1_SCE_PREEMPPRIORITY, CAN1_SCE_SUBPRIORITY);
+	HAL_NVIC_EnableIRQ(CAN1_SCE_IRQn);
+}
 
 /**
   * @brief  FreeRTOS initialization for bxCAN module
@@ -119,70 +158,5 @@ void MX_CAN1_Init(void)
   if (HAL_CAN_Init(&hcan1) != HAL_OK)
   {
     Error_Handler();
-  }
-  /* USER CODE BEGIN CAN1_Init 2 */
-
-  /* USER CODE END CAN1_Init 2 */
-
-}
-
-void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
-{
-
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspInit 0 */
-
-  /* USER CODE END CAN1_MspInit 0 */
-    /* CAN1 clock enable */
-    __HAL_RCC_CAN1_CLK_ENABLE();
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**CAN1 GPIO Configuration
-    PA11     ------> CAN1_RX
-    PA12     ------> CAN1_TX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF9_CAN1;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    /* CAN1 interrupt Init */
-    HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
-    HAL_NVIC_SetPriority(CAN1_SCE_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(CAN1_SCE_IRQn);
-  /* USER CODE BEGIN CAN1_MspInit 1 */
-
-  /* USER CODE END CAN1_MspInit 1 */
-  }
-}
-
-void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
-{
-
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspDeInit 0 */
-
-  /* USER CODE END CAN1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_CAN1_CLK_DISABLE();
-
-    /**CAN1 GPIO Configuration
-    PA11     ------> CAN1_RX
-    PA12     ------> CAN1_TX
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
-
-    /* CAN1 interrupt Deinit */
-    HAL_NVIC_DisableIRQ(CAN1_RX0_IRQn);
-    HAL_NVIC_DisableIRQ(CAN1_SCE_IRQn);
-  /* USER CODE BEGIN CAN1_MspDeInit 1 */
-
-  /* USER CODE END CAN1_MspDeInit 1 */
   }
 }
