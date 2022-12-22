3ea053ce74dad89ce895d8318e9df238c764bc18
Vlad Shumeiko <wladshumeiko228@gmail.com>
2022-12-22 13:47:32 +0100

    [freertos.c] feat: delete default task

--- STM32+FreeRTOS+bxCAN/Core/Src/freertos.c
+++ STM32+FreeRTOS+bxCAN/Core/Src/freertos.c
@@ -5,12 +5,9 @@
 #include "task.h"
 #include "main.h"
 
-
 //---------------------------------------------------------------------------
 // Functions prototypes
 //---------------------------------------------------------------------------
-osThreadId defaultTaskHandle;
-void StartDefaultTask(void const * argument);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 // GetIdleTaskMemory prototype (linked to static allocation support)
@@ -42,26 +39,4 @@ void MX_FREERTOS_Init(void)
 	HEARTBEAT_FreeRTOS_init();
 #endif
 
-
-	/* Create the thread(s) */
-	/* definition and creation of defaultTask */
-	osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
-	defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
-}
-
-/**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void const * argument)
-{
-  /* USER CODE BEGIN StartDefaultTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END StartDefaultTask */
 }
