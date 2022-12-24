0a49b79db160715305406f28239a0ddbcc721cdb
Vlad Shumeiko <wladshumeiko228@gmail.com>
2022-12-24 18:05:09 +0100

    [FreeRTOSConfig.h] feat: change length of task name

--- STM32+FreeRTOS+bxCAN/Core/Inc/FreeRTOSConfig.h
+++ STM32+FreeRTOS+bxCAN/Core/Inc/FreeRTOSConfig.h
@@ -23,7 +23,7 @@
 #define configTICK_RATE_HZ                       		((TickType_t)1000)
 #define configMAX_PRIORITIES                     		( 7 )
 #define configMINIMAL_STACK_SIZE                 		((uint16_t)128)
-#define configMAX_TASK_NAME_LEN                  		( 16 )
+#define configMAX_TASK_NAME_LEN                  		( 20 )
 #define configUSE_16_BIT_TICKS                   		0
 #define configIDLE_SHOULD_YIELD                 		1
 #define configUSE_TASK_NOTIFICATIONS            		1
