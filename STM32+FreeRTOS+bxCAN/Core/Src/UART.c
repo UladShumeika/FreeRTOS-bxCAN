9adb169f005a565df8958f37e653a0d3cfb655eb
Vlad Shumeiko <wladshumeiko228@gmail.com>
2022-12-26 20:03:18 +0100

    [UART.c] feat: create UARTSendingMessages task

--- STM32+FreeRTOS+bxCAN/Core/Src/UART.c
+++ STM32+FreeRTOS+bxCAN/Core/Src/UART.c
@@ -10,10 +10,26 @@
 //---------------------------------------------------------------------------
 // Descriptions of FreeRTOS elements
 //---------------------------------------------------------------------------
+static osThreadId UARTSendingMessagesHandle;
 
 //---------------------------------------------------------------------------
 // FreeRTOS's threads
 //---------------------------------------------------------------------------
+
+/**
+* @brief Function implementing the UARTSendingMessages thread.
+* @param argument: Not used
+* @retval None
+*/
+void UARTSendingMessagesTask(void const* argument)
+{
+
+	/* Infinite loop */
+	for(;;)
+	{
+		osDelay(1);
+	}
+}
 
 //---------------------------------------------------------------------------
 // Initialization functions
@@ -26,5 +42,8 @@
   */
 void UART_FreeRTOS_init(void)
 {
-
+	// Create the thread(s)
+	// definition and creation of UARTSendingMessagesTask
+	osThreadDef(UARTSending, UARTSendingMessagesTask, osPriorityAboveNormal, 0, 128);
+	UARTSendingMessagesHandle = osThreadCreate(osThread(UARTSending), NULL);
 }
