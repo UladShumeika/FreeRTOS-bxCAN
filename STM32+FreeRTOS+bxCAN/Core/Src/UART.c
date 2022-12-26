49a7a655f48014e67069ceb800f4e6300b6cde3d
Vlad Shumeiko <wladshumeiko228@gmail.com>
2022-12-26 20:46:28 +0100

    [UART.c] feat: add UART_USART_init function

--- STM32+FreeRTOS+bxCAN/Core/Src/UART.c
+++ STM32+FreeRTOS+bxCAN/Core/Src/UART.c
@@ -4,8 +4,14 @@
 #include "UART.h"
 
 //---------------------------------------------------------------------------
+// Typedefs
+//---------------------------------------------------------------------------
+static UART_HandleTypeDef huart1;
+
+//---------------------------------------------------------------------------
 // Static function prototypes
 //---------------------------------------------------------------------------
+static void UART_USART_init(void);
 
 //---------------------------------------------------------------------------
 // Descriptions of FreeRTOS elements
@@ -23,6 +29,7 @@ static osThreadId UARTSendingMessagesHandle;
 */
 void UARTSendingMessagesTask(void const* argument)
 {
+	UART_USART_init();
 
 	/* Infinite loop */
 	for(;;)
@@ -34,6 +41,38 @@ void UARTSendingMessagesTask(void const* argument)
 //---------------------------------------------------------------------------
 // Initialization functions
 //---------------------------------------------------------------------------
+
+/**
+  * @brief  USART configuration UART module
+  * @param  None
+  * @retval None
+  */
+static void UART_USART_init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	__HAL_RCC_USART1_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	// USART1 GPIO Configuration
+	// PA9     ------> USART1_TX
+	GPIO_InitStruct.Pin 						= GPIO_PIN_9;
+	GPIO_InitStruct.Mode 						= GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull 						= GPIO_NOPULL;
+	GPIO_InitStruct.Speed 						= GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Alternate 					= GPIO_AF7_USART1;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	huart1.Instance 							= USART1;
+	huart1.Init.BaudRate 						= 115200;
+	huart1.Init.WordLength 						= UART_WORDLENGTH_8B;
+	huart1.Init.StopBits 						= UART_STOPBITS_1;
+	huart1.Init.Parity 							= UART_PARITY_NONE;
+	huart1.Init.Mode 							= UART_MODE_TX;
+	huart1.Init.HwFlowCtl 						= UART_HWCONTROL_NONE;
+	huart1.Init.OverSampling 					= UART_OVERSAMPLING_16;
+	HAL_UART_Init(&huart1);
+}
 
 /**
   * @brief  FreeRTOS initialization for UART module
